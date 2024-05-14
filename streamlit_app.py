8549edb4b47212b92405621eae67b348bd3948ee
jackhulbertpdx <39444980+jackhulbertpdx@users.noreply.github.com>
2024-05-14 21:59:23 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 """
-# Hey Ross, here's some data on pools in Austin from [the city's public data API]https://data.austintexas.gov/
+# Hey Ross, here's some data on pools in Austin from the city's public data API.
 """
 
 st.components.v1.iframe("https://public.tableau.com/views/pools/Dashboard1?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link", width=1200, height=600, scrolling=true)
